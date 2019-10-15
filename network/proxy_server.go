a73d6b36f198fb2659a1c00ebda5ea0f1360bc98
Andy <LL3707@outlook.com>
2019-10-15 15:16:54 +0800

    add files

---
+++ network/proxy_server.go
@@ -0,0 +1,104 @@
+package network
+
+import (
+	"bytes"
+	"consensusPBFT/pbft/consensus"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Server struct {
+	url  string
+	node *Node
+}
+
+func NewServer(nodeID string) *Server {
+	node := NewNode(nodeID)
+	server := &Server{node.NodeTable[nodeID], node}
+
+	server.setRoute()
+	return server
+}
+
+func (server *Server) Start() {
+	fmt.Printf("Server will be started at %s...\n", server.url)
+	if err := http.ListenAndServe(server.url, nil); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (server *Server) setRoute() {
+	http.HandleFunc("/req", server.getReq)
+	http.HandleFunc("/preprepare", server.getPrePrepare)
+	http.HandleFunc("/prepare", server.getPrepare)
+	http.HandleFunc("/commit", server.getCommit)
+	http.HandleFunc("/reply", server.getReply)
+}
+
+func (server *Server) getReq(writer http.ResponseWriter, request *http.Request) {
+	var msg consensus.RequestMsg
+	err := json.NewDecoder(request.Body).Decode(&msg)
+	if err != nil {
+		fmt.Println("err msg")
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("------------req msg-----------")
+	fmt.Println(msg)
+	fmt.Println("------------req-----------")
+	server.node.MsgEntrance <- &msg // 把信息丢到通道中
+}
+
+func (server *Server) getPrePrepare(writer http.ResponseWriter, request *http.Request) {
+	var msg consensus.PrePrepareMsg
+	err := json.NewDecoder(request.Body).Decode(&msg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	server.node.MsgEntrance <- &msg
+}
+
+func (server *Server) getPrepare(writer http.ResponseWriter, request *http.Request) {
+	var msg consensus.VoteMsg
+	err := json.NewDecoder(request.Body).Decode(&msg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	server.node.MsgEntrance <- &msg
+}
+
+func (server *Server) getCommit(writer http.ResponseWriter, request *http.Request) {
+	//fmt.Println("----------	(server *Server) getCommit-----------")
+	var msg consensus.VoteMsg
+	err := json.NewDecoder(request.Body).Decode(&msg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	server.node.MsgEntrance <- &msg
+}
+
+func (server *Server) getReply(writer http.ResponseWriter, request *http.Request) {
+	//fmt.Println("----------	(server *Server) getReply-----------")
+	var msg consensus.ReplyMsg
+	err := json.NewDecoder(request.Body).Decode(&msg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	server.node.GetReply(&msg)
+}
+
+func send(url string, msg []byte) {
+	buff := bytes.NewBuffer(msg)
+	//fmt.Println("-----------", buff, "-----------")
+	http.Post("http://"+url, "application/json", buff)
+}
